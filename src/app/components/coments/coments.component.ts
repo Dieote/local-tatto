c685b9512cba2ca366a5defa561650ec43298fbf
Dieote <gomezmdiego97@gmail.com>
2023-07-09 15:28:21 +0200

    Correccion de comments

--- src/app/components/coments/coments.component.ts
+++ src/app/components/coments/coments.component.ts
@@ -12,12 +12,13 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./coments.component.scss'],
 })
 export class ComentsComponent implements OnInit {
-  newComment: Comment = new Comment('');
+  newComment: Comment = new Comment('', '');
   arrayComentario: Comment[] = [];
-  rtaPost: string = '';
+  rtaPost: any;
 
   form: FormGroup = new FormGroup({
     formComentario: new FormControl(''),
+    formUsername: new FormControl(''),
   });
 
   constructor(
@@ -28,6 +29,7 @@ export class ComentsComponent implements OnInit {
 
   ngOnInit(): void {
     this.subscribeArrayComment();
+    this.form.addControl('formUsername', new FormControl(''));
   }
 
   subscribeArrayComment() {
@@ -37,48 +39,41 @@ export class ComentsComponent implements OnInit {
   }
 
   sendComent() {
-    let commentPost = new Comment(this.form.value.formComentario || '');
+    let commentPost = new Comment(
+      this.form.value.formComentario || '',
+      this.form.value.formUsername || ''
+    );
 
     this.callSuccesToastr('Comentario enviado correctamente');
     this.commentsService.createComment(commentPost).subscribe((data) => {
       this.rtaPost = data;
 
-      if (data === 'Funciona') {
+      if (data) {
         this.subscribeArrayComment();
       }
     });
-    this.newComment = new Comment('');
+    this.form.reset();
   }
 
   callSuccesToastr(mensaje: string) {
     this.toastr.success(mensaje);
   }
+
   deleteComent(id: number): void {
-    this.toastr.success('Comentario borrado correctamente.', 'titulo', {
-      progressBar: true,
-    });
-    this.commentsService.deleteComment(id).subscribe(() => {
-      this.subscribeArrayComment();
-    });
+    this.commentsService.deleteComment(id).subscribe(
+      (data: any) => {
+        this.toastr.success('Comentario eliminado correctamente.');
+        console.log(data.message);
+        this.refresh();
+      },
+      (error) => {
+        this.toastr.error('Error al eliminar el comentario.');
+        console.error('Error al eliminar el comentario:', error);
+      }
+    );
   }
 
-  sendToEmail(comment: Comment) {
-    const text = {
-      to: 'correo@empresatatto.com',
-      subject: 'Opinion de usuario',
-      body: comment,
-    };
-    //esto deberia ir al service
-    //this.http.post('https://api.correo.com/enviar', text).subscribe(
-    //   () => {
-    //     console.log('Comentario enviado por correo electrónico.');
-    //   },
-    //   (error) => {
-    //     console.error(
-    //       'Error al enviar el comentario por correo electrónico:',
-    //       error
-    //     );
-    //   }
-    // );
+  refresh(): void {
+    this.subscribeArrayComment();
   }
 }
